c487d760d15da4b7dd2a0937b437f101043c36bb
T Lucas <107696223+TamLucas@users.noreply.github.com>
2023-01-23 15:58:52 -0600

    Add files via upload

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the diameter of your desired pizza: ");
+            float diameter = Console.ReadLine();
+
+            double pi = 3.14159;
+            double pizzaSliceArea = 14.125;
+            double pizzaTotalArea = (pi *  Math.Pow(r, 2));
+            float r = diameter / 2;
+
+            float calculate = (diameter * pizzaSliceArea);
+            Console.WriteLine("The number of slices will be " + calculate);
+
+            if(calculate == 3 or 4 or 5)
+            {
+                Console.WriteLine("It's a small pizza");
+            }
+            if(calculate == 6 or 7 or 8)
+            {
+                Console.WriteLine("It's a medium pizza");
+            }
+            if(calculate == 9 or 10)
+            {
+                 Console.WriteLine("It's a large pizza");
+            }
+            else
+            {
+                Console.WriteLine("Size not supported");
+            }
+        }
+
+
+    }
+}
